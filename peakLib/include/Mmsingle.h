3e5cf2cc5b2a061026a7234cfb4227dbed0f862e
wddpeakking <wddpeakking@sina.com>
2016-02-02 21:39:44 +0800

    add singleton

--- peakLib/include/Mmsingle.h
+++ peakLib/include/Mmsingle.h
@@ -7,7 +7,19 @@ class CSingleInstace
 public:
 	CSingleInstace(){};
 	~CSingleInstace(){};
+	T* GetInstance()
+	{
+		if(g_Instace == NULL)
+		{
+			g_Instace = new T;
+		}
+		return g_Instace;
+	}
+private:
+	static T* g_Instace;
 
 }
 
+template<class T> 
+CSingleInstace<T> CSingleInstace::g_Instance = NULL;
 #endif

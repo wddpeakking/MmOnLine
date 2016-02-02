98d58cf13d1e301a20ede501f3be918ffd65dcf7
wddpeakking <wddpeakking@sina.com>
2016-02-02 22:02:10 +0800

    add ret code

--- peakLib/include/MmError.h
+++ peakLib/include/MmError.h
@@ -1,8 +1,20 @@
-#pragma once
-class CMmError
+#ifndef _Manm_Error_
+#define _Manm_Error_
+
+#include "Mmsingle.h"
+
+class CMmError : public CSingleton<CMmError>
 {
 public:
 	CMmError(void);
 	~CMmError(void);
+
+	enum Error_Code
+	{
+		en_OK,
+		en_False,
+	};
 };
+
+#endif
 

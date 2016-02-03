a90b983582210835a5dad4720139293cab999c91
wddpeakking <wddpeakking@sina.com>
2016-02-03 17:02:12 +0800

    添加线程管理类

--- peakLib/include/MmThread.h
+++ peakLib/include/MmThread.h
@@ -1,11 +1,22 @@
 #ifndef _Manm_Thread_H_
 #define _Manm_Thread_H_
-
+#include "MmTypedef.h"
 class CMmThread
 {
 public:
 	CMmThread(void);
 	~CMmThread(void);
+
+	virtual void CreateThread();
+	virtual bool IsRunning();
+	virtual void Stop(); 
+
+	virtual void run()=0;
+	 
+private:
+	void* m_pHandle;		//线程句柄
+	uint32 m_nThreadID;		//线程ID
+	bool m_bIsRunning;		//运行状态
 };
 
 #endif
--- peakLib/src/MmThread.cpp
+++ peakLib/src/MmThread.cpp
@@ -1,12 +1,53 @@
 #include "../include/MmThread.h"
 #include "../include/MmLog.h"
 
+#ifdef _WINDOWS_OS_
+#include <Windows.h>
+#else
+
+#endif
+static unsigned long __stdcall ProxyFunc(void* pFun)
+{
+	if (pFun)
+	{
+		CMmThread* pThread = (CMmThread*)(pFun);
+		if (pThread)
+		{
+			pThread->run();
+			return 0;
+		}
+	}
+	return -1;
+}
+
 CMmThread::CMmThread(void)
 {
-	LogInfoMsg("%s","thread");
+
 }
 
 
 CMmThread::~CMmThread(void)
 {
+}
+
+void CMmThread::CreateThread()
+{
+#ifdef _WINDOWS_OS_
+	m_pHandle = (void*)::CreateThread(NULL,0,ProxyFunc,this,0,(DWORD*)&m_nThreadID);
+	SetThreadPriorityBoost(m_pHandle,TRUE);
+	if (m_pHandle == NULL)
+	{
+		LogErrorMsg("Create Thread Failed");
+	}
+#else
+
+#endif
+}
+bool CMmThread::IsRunning()
+{
+	return m_bIsRunning;
+}
+void CMmThread::Stop()
+{
+	m_bIsRunning = false;
 }
